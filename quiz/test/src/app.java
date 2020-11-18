e9f8c6804dd5c0c139702c14ab6dd7c2c2720c61
Nikamezvrishvili <nikamezvrishvili18@gmail.com>
2020-11-19 01:31:15 +0400

    OTT
    Object->text

--- quiz/test/src/app.java
+++ quiz/test/src/app.java
@@ -1,33 +1,64 @@
+
+import java.io.FileOutputStream;
+
 import java.util.Scanner;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class app {
+
+
     public static void main(String[] args) {
-        String q1="What color are apples?\n"+"(a)red/green\n(b)orange\n(c)magenta\n";
+        String q1="What color are apples?\n"+"(a)red/green\n(b)orange\n(c)black\n";
         String q2="What color are bananas?\n"+"(a)red/green\n(b)yellow\n(c)blue\n";
+        String filename = "test.bin";
+        try {
+            ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(filename));
+            os.writeObject(q1);
+
+            os.writeObject(q2);
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
         Question[] questions = {
                 new Question(q1,"a"),
                 new Question(q2,"b")
         };
-        takeTest(questions);
+        try {
+            takeTest(questions);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
 
-    public static void takeTest(Question[]questions){
+    public static void takeTest(Question[]questions) throws IOException {
         int score = 0;
+
         Scanner keyboardInput = new Scanner(System.in);
-        for(int i =0; i < questions.length; i++){
+        int i =0;
+        while (i < questions.length) {
             System.out.println(questions[i].prompt);
             String answer = keyboardInput.nextLine();
             if(answer.equals(questions[i].answer)){
                 score++;
             }
 
+            i++;
         }
         System.out.println("You got " + score + "/" + questions.length);
+        System.out.println("Done Writing");
 
-    }
+
+
+
+
+   }
 
 }
